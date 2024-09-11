c6fcc8b9256e044752070a341ebf1f4999e2be2f
LAPTOP-G1SP4EVV\pasha <pashashershnev@gmail.com>
2024-09-10 20:19:33 -0400

    warofdrawing

--- app/src/main/java/com/example/towerbit/MainMenu.kt
+++ app/src/main/java/com/example/towerbit/MainMenu.kt
@@ -7,27 +7,45 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import android.view.View
+import android.widget.ImageView
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_menu)
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_FULLSCREEN       // Полноэкранный режим (скрытие статусной строки)
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // Скрытие навигационной панели
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) // Восстановление системных панелей при свайпе
+
+
+        // Обрабатываем WindowInsets для корректной работы с системными панелями
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            // Возвращаем insets, чтобы ошибка исчезла
             insets
         }
 
+        // Анимация изображения
+        val animatedImage = findViewById<ImageView>(R.id.main_image)
+        val animationDrawable = AnimationDrawable()
 
-        // Найдем кнопку по её ID
-        val button = findViewById<Button>(R.id.StartButton)
+        // Добавляем кадры
+        animationDrawable.addFrame(getDrawable(R.drawable.fs0)!!, 100)
+        animationDrawable.addFrame(getDrawable(R.drawable.fs1)!!, 100)
+        animationDrawable.addFrame(getDrawable(R.drawable.fs2)!!, 100)
+        animationDrawable.addFrame(getDrawable(R.drawable.fs3)!!, 100)
 
-        // Устанавливаем слушатель нажатия
-        button.setOnClickListener {
-            // Создаем намерение для перехода на новую активность
-            val intent = Intent(this, MainMap::class.java)
-            startActivity(intent) // Стартуем новую активность
-        }
+        // Устанавливаем анимацию для ImageView
+        animatedImage.setImageDrawable(animationDrawable)
+
+        // Запускаем анимацию
+        animationDrawable.isOneShot = false
+        animatedImage.post { animationDrawable.start() }
     }
 }
